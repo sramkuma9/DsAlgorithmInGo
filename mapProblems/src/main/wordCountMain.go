f4b8ddcfcfcb4e46c7a2dffcd27828ef52882838
sramkuma9 <ajay.rsin@gmail.com>
2021-08-17 21:32:33 +0530

    Test the integration between vs code and github

--- mapProblems/src/main/wordCountMain.go
+++ mapProblems/src/main/wordCountMain.go
@@ -7,5 +7,5 @@ import (
 
 func main() {
 	outputWordCount := src.WordCount("When the going gets tough the tough gets going")
-	fmt.Println("Count of words from a sentence is ", outputWordCount)
+	fmt.Println("Count of words from a sentence is :", outputWordCount)
 }
